ffe851fb2397e7380afea1ffc26c83a1fa48d409
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2024-08-15 20:36:15 +0700

    router vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1 class="text-red-500 font-bold">hello World</h1>
+  <div id="app">
+    <nav>
+      <router-link to="/">Trang Chủ</router-link>
+      <router-link to="/about">Giới Thiệu</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
