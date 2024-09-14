03ef1fdf05a1874048e0e4748c9bd32e420d971a
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2024-09-14 21:21:17 +0700

    homepage 30%

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,117 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Trang Chủ</router-link>
-      <router-link to="/about">Giới Thiệu</router-link>
-    </nav>
+    <header>
+      <div
+        class="relative flex items-center justify-between lg:py-4 lg:px-20 p-4"
+      >
+        <div class="px-4">
+          <h1
+            class="font-bold lg:text-2xl text-[14px] uppercase text-[#4169E1]"
+          >
+            <i class="fa-solid fa-book"></i> - BookStore
+          </h1>
+        </div>
+        <div class="lg:flex hidden flex-1 gap-4 max-w-xl">
+          <input
+            type="text"
+            class="items-center w-full p-4 border border-gray-400 text-base font-semibold tracking-wider text-black bg-white/10 rounded-lg focus:outline-none"
+            placeholder="Tìm kiếm tên sách ..."
+          />
+          <button
+            class="font-bold text-lg bg-[#4169E1] text-white px-4 py-2 rounded-lg whitespace-nowrap"
+          >
+            Tìm kiếm
+          </button>
+        </div>
+        <ul class="lg:flex hidden">
+          <li class="px-[15px] lg:px-[26px] text-[20px] group">
+            <a href="" class="font-bold">Lịch sử mượn sách</a>
+            <div
+              class="h-[2px] bg-[#4169E1] scale-x-0 group-hover:scale-100 rounded-full transition-all ease-out origin-left duration-500"
+            ></div>
+          </li>
+        </ul>
+        <div class="flex items-center lg:space-x-8 space-x-5 px-4">
+          <div
+            class="flex space-x-4 items-center justify-center cursor-pointer"
+          >
+            <img
+              src="../src/assets/img/avatar-1.jpg"
+              class="lg:w-[50px] lg:h-[50px] w-[40px] h-[40px] rounded-full border-2 border-[#C0C0C0]"
+              alt=""
+            />
+          </div>
+          <div>
+            <div class="relative">
+              <div
+                class="absolute top-[-12px] right-[-10px] flex justify-center items-center w-6 h-6 bg-[#4169E1] rounded-full"
+              >
+                <span class="text-white font-bold text-base">1</span>
+              </div>
+              <button>
+                <i class="fa-solid fa-cart-shopping text-[26px]"></i>
+              </button>
+            </div>
+          </div>
+          <button class="open-menu lg:hidden block">
+            <i class="fa-solid fa-bars text-[26px] text-[#4169E1]"></i>
+          </button>
+        </div>
+        <div
+          class="sidebar fixed top-0 -left-[100%] bg-[#fff] p-4 w-full h-full z-40"
+        >
+          <h1
+            class="font-bold text-xl uppercase text-[#4169E1]"
+          >
+            <i class="fa-solid fa-book"></i> - BookStore
+          </h1>
+          <button class="closed">
+            <i
+              class="fa-solid fa-x absolute top-3 right-4 text-2xl text-[#4169E1] p-4 ease-out duration-[0.4s] hover:scale-[1.1]"
+            ></i>
+          </button>
+          <ul class="flex flex-col my-10">
+            <form action="" method="post" class="relative mb-4 flex space-x-4">
+              <input
+                type="text"
+                class="items-center w-full p-4 border border-gray-400 text-base font-semibold tracking-wider text-black bg-white/10 rounded-lg focus:outline-none"
+                placeholder="Tìm kiếm tên sách ..."
+              />
+              <button
+                class="font-bold text-lg bg-[#4169E1] text-white px-4 py-2 rounded-lg whitespace-nowrap"
+              >
+                Tìm kiếm
+              </button>
+            </form>
+            <li class="pb-[15px]">
+              <a
+                href=""
+                class="border-b-2 border-transparent hover:border-b-2 hover:border-[#4169E1] font-semibold text-[20px] hover:text-[#4169E1] transition-colors duration-300"
+                >Lịch sử mượn sách</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-};
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const openMenu = $(".open-menu");
+  const closeMenu = $(".closed");
+  const sidebar = $(".sidebar");
+
+  openMenu.on("click", function () {
+    sidebar.animate({ left: "0" }, 400);
+  });
+
+  closeMenu.on("click", function () {
+    sidebar.animate({ left: "-100%" }, 400);
+  });
+});
 </script>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const images = ref(['/src/assets/img/home-book-4.png', '/src/assets/img/home-book-2.png', '/src/assets/img/home-book-3.png']);
+</script>
 <template>
-  <h1>Home Page</h1>
+  <div class="lg:py-20 lg:px-24 p-4">
+    <div class="flex flex-col gap-7 lg:flex-row bg-[#4169E1] text-white p-10 rounded-2xl shadow-md overflow-hidden">
+      <div class="lg:w-[40%] w-full flex flex-col lg:items-start justify-center space-y-4">
+        <p class="font-bold lg:text-2xl text-xl text-center">JeiKei Library</p>
+        <h1 class="font-bold lg:text-[50px] text-[30px] text-center">Online BookStore</h1>
+        <p class="text-sm lg:text-lg font-medium text-black text-center lg:text-left">
+          Nếu bạn là một người yêu thích việc đọc sách thì website này chính là
+          nơi để bạn thõa mãn được đam mê của mình. JeiKei Library - nơi những
+          cuốn sách trở nên sống động và ý nghĩa hơn bao giờ hết.
+        </p>
+        <p class="text-sm lg:text-lg font-medium text-black text-center">Liên hệ: <span class="text-white">079-965-8592</span></p>
+        <button class="lg:px-8 lg:py-4 px-6 py-4 bg-white text-[#4169E1] text-base lg:text-xl font-semibold rounded-xl shadow-md">Read More</button>
+      </div>
+      <div class="w-full flex items-center justify-center">
+        <img v-for="(img, index) in images" class="lg:w-[300px] w-[150px]" :class="{ 'lg:w-[340px] w-[190px]': index % 2 !== 0 }" :key="index" :src="img" alt="">    
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-    name: 'Home'
-}
-</script>
-
 <style>
-
 </style>
